2c2f7e530bac95ea217941cd6c8d8341ea7639c4
Esra Işık <35555945+onceinabluemoon@users.noreply.github.com>
2019-07-10 15:39:51 +0300

    Update selenium_facebook.py

--- selenium_facebook.py
+++ selenium_facebook.py
@@ -4,22 +4,18 @@ import time
 browser = webdriver.Chrome()
 
 browser.get("https://www.facebook.com")
-time.sleep(5)
-
+time.sleep(3)
 
 # xpaths: //*[@id="email"]  & //*[@id="pass"]
 email = browser.find_element_by_xpath('//*[@id="email"]')
 password = browser.find_element_by_xpath('//*[@id="pass"]')
 
-
 email.send_keys("")         # You have to add e-mail or phone number
 password.send_keys("")      # add your password              
 time.sleep(3)
 
-
 login = browser.find_element_by_id('loginbutton')
 login.click()
 
-time.sleep(1)
-
+time.sleep(3)
 browser.quit()
